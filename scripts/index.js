903b12c9aaef3bac9628ca2661b33e7487901213
silvester <silvesterngaruiya7@gmail.com>
2025-03-18 12:44:45 +0300

    adde changes to my javascript

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,9 @@
 const ramens = [
-  {
-    id: 1,
-    name: "Shoyu Ramen",
-    restaurant: "Ichiran",
-    image: "shoyu.jpg",
-    rating: 5,
-    comment: "Delicious!",
-  },
-  {
-    id: 2,
-    name: "Miso Ramen",
-    restaurant: "Menya",
-    image: "miso.jpg",
-    rating: 4,
-    comment: "Very flavorful!",
-  },
-  {
-    id: 3,
-    name: "Tonkotsu Ramen",
-    restaurant: "Ramen-ya",
-    image: "tonkotsu.jpg",
-  },
+  { img: "./images/image copy 2.png", name: "Tonokotsu Ramen" },
+  { img: "./images/image copy 3.png", name: "Shio Ramen" },
+  { img: "./images/image copy 5.png", name: "solitos Ramen" },
+  { img: "./images/image copy.png", name: "Miso Ramen" },
+  { img: "./images/image.png", name: "Shoyu Ramen" },
 ];
 
 // displaying ramens in the ramens menu
@@ -28,37 +11,45 @@ const ramens = [
 const displayRamen = () => {
   let section = document.getElementById("ramen-menu");
 
-    ramens.forEach((ramen) => {
-      let img = document.createElement("img");
-      img.src = ramen.image;
-      img.alt = ramen.name;
-      img.style.cursor = "pointer";
+  ramens.forEach((ramen) => {
+    let img = document.createElement("img");
+    img.src = ramen.img;
+    img.alt = ramen.name;
+    img.style.cursor = "pointer";
 
-      img.addEventListener("click", () => showRamenDetails(ramen));
-      section.appendChild(img);
-    });
+    img.addEventListener("click", () => showRamenDetails(ramen));
+    section.appendChild(img);
+  });
 
-  console.log("this is my section", section);
+  console.log(" my section", section);
 };
 
 const handleClick = () => {
   // get the details of the ramen and display it in the ramen-detail section
+  console.log(" clicked");
+  
 };
 
 const showRamenDetails = (ramen) => {
   // get the details of the ramen and display it in the ramen-detail section
+  let section = document.getElementById("ramen-detail");  
+  section.innerHTML = "";
+  let img = document.createElement("img");
+  img.src = ramen.img;
+  img.alt = ramen.name;
+  section.appendChild(img);
+
 };
 
 const createNewRamen = () => {
-  name = document.getElementById("name").value;
-  restaurant = document.getElementById("restaurant").value =
-    document.getElementById("image").value;
-  image = document.getElementById("image").value;
-  rating = document.getElementById("rating").value;
-  comment = document.getElementById("comment").value;
+  name = document.getElementById("new-name").value;
+  restaurant = document.getElementById("new-restaurant").value;
+  image = document.getElementById("new-image").value;
+  rating = document.getElementById("new-rating").value;
+  comment = document.getElementById("new-comment").value;
 
-  const newRamen = {
-    id: ramens.length + 1,
+  let newRamen = {
+    // id: ramens.length + 1,
     name: name,
     restaurant: restaurant,
     image: image,
@@ -73,6 +64,8 @@ const createNewRamen = () => {
   });
 };
 const ramenDetails = document.getElementById("ramen-detail");
+
+const form = document.getElementById("new-ramen");
 
 ramenDetails.addEventListener(`click`, () => {
   handleClick();
